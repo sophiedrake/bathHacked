c1e3ec6cf2f73e7a5dbd9189765ae86ff03b606f
Sophie Drake <sophiedrake351@gmail.com>
2014-11-01 18:49:52 +0000

    removed unneeded bits from main.py

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 DATABASE = os.path.join(app.root_path, 'sqlite_db.db')
 
-
-
 @app.route('/')
 def show_landing():
     return render_template('home.html')
@@ -40,108 +38,11 @@ def show_league():
 def show_about():
     return render_template('about.html')
 
-@app.route('/testing')
-def show_testing():
-    p = "BA2 3PB"
-    postcodes = query_db('select score from Score where Postcode= \"' + p + '\"')
-    print postcodes
-    return render_template('testing.html', postcodes=postcodes)
-
-@app.route('/map', methods=['POST'])
-def show_map():
-    p = request.form['address']
-    p = p.upper()
-    p = p.replace(" ", "")
-    
-    recycling = query_db('select score from score where Postcode= \"' + p + '\" and Metric = \"Recycling\"')
-    if len(recycling) >= 1:
-      recycling = recycling[0][0]
-    if recycling == []:
-      ward = find_ward_by_postcode(p)
-      if ward is None:
-        recycling = "?"
-      else:
-        recycling = query_db('select score from score where Ward= \"' + ward[0] + '\" and Metric = \"Recycling\"')
-        if len(recycling) >= 1:
-          recycling = recycling[0][0]
-    if recycling is None:
-      recycling = "?"
-     
-    gas = query_db('select score from score where Postcode= \"' + p + '\" and Metric = \"Gas\"')
-    if len(gas) >= 1:
-      gas = gas[0][0]
-    if gas == []:
-      ward = find_ward_by_postcode(p)
-      if ward is None:
-        gas = "?"
-      else:
-        gas = query_db('select score from score where Ward= \"' + ward[0] + '\" and Metric = \"Gas\"')
-        if len(gas) >= 1:
-          gas = gas[0][0]
-    if gas is None:
-      gas = "?"
-
-    electricity = query_db('select score from score where Postcode= \"' + p + '\" and Metric = \"Electricity\"')
-    if len(electricity) >= 1:
-      electricity = electricity[0][0]
-    if electricity == []:
-      ward = find_ward_by_postcode(p)
-      if ward is None:
-        electricity = "?"
-      else:
-        electricity = query_db('select score from score where Ward= \"' + ward[0] + '\" and Metric = \"Electricity\"')
-        if len(electricity) >= 1:
-          electricity = electricity[0][0]
-    if electricity is None:
-      electricity = "?"
-      
-    transport = query_db('select score from score where Postcode= \"' + p + '\" and Metric = \"Transport\"')
-    if len(transport) >= 1:
-      transport = transport[0][0]
-    if  transport  == []:
-      ward = find_ward_by_postcode(p)
-      if ward is None:
-         transport  = "?"
-      else:
-        transport  = query_db('select score from score where Ward= \"' + ward[0] + '\" and Metric = \"Transport\"')
-        
-        if len(transport) >= 1:
-          transport = transport[0][0]
-        if  transport  == []:
-          transport = "?"
-    if  transport  is None:
-       transport  = "?"
-
-    final = query_db('select score from score where Postcode= \"' + p + '\" and Metric = \"Final\"')
-    if len(final) >= 1:
-      final = final[0][0]
-    if  final  == []:
-      ward = find_ward_by_postcode(p)
-      if ward is None:
-         final  = "?"
-      else:
-        final  = query_db('select score from score where Ward= \"' + ward[0] + '\" and Metric = \"Final\"')
-        if len(final) >= 1:
-          final = final[0][0]
-        if  final  == []:
-          final = "?"
-    if  final  is None:
-       final  = "?"
-
-    
-    
-    return render_template('map.html', address=p, recycling=recycling, gas=gas, electricity=electricity, transport=transport, final=final)
-
-@app.route('/more')
-def more():
-    return render_template('more.html')
-
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
 
 #Fires up the server
 if __name__ == '__main__':
